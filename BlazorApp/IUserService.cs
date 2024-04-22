e41728f54f4c3aedf3786c4ca6a69e232a598405
lizakitte <lizakitte@gmail.com>
2024-04-22 14:08:55 +0200

    blazor chat

---
+++ BlazorApp/IUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp
+{
+    public interface IUserService
+    {
+        public void Add(string connectionId, string username);
+        public void RemoveByName(string username);
+        public string GetConnectionIdByName(string username);
+        public IEnumerable<(string ConnectionId, string Username)> GetAll();
+    }
+}
---
+++ BlazorApp/UserService.cs
@@ -0,0 +1,40 @@
+﻿namespace BlazorApp
+{
+    public class UserService : IUserService
+    {
+        private Dictionary<string, string> users = new Dictionary<string, string>();
+
+        public void Add(string connectionId, string username)
+        {
+            users.Add(connectionId, username);
+        }
+
+        public IEnumerable<(string ConnectionId, string Username)> GetAll()
+        {
+            return users.Select(u => (u.Key, u.Value));
+        }
+
+        public string GetConnectionIdByName(string username)
+        {
+            foreach (var (id, name) in users)
+            {
+                if(name == username)
+                {
+                    return id;
+                }
+            }
+            return "";
+        }
+
+        public void RemoveByName(string username)
+        {
+            foreach (var (id, name) in users)
+            {
+                if (name == username)
+                {
+                    users.Remove(id);
+                }
+            }
+        }
+    }
+}
